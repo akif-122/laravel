88357631b6522a5ff23a991310af6c1c86ae4b46
Akif Ullah <akifullah0317@gmail.com>
2024-07-29 10:26:21 -0700

    Custom validation rule

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -11,10 +11,21 @@ class FormController extends Controller
     function add(Request $req){
         $req->validate([
             "name"=> "required | min:3 | max:10 ",
-            "city"=> "required  ",
+            "city"=> "required | uppercase ",
             "phone"=> "required | min:10 | max: 11",
             "skill"=> "required",
             "age"=> "required ",
+        ],[
+            "name.required"=>"Name is Required",
+            "name.min"=>"Name minimum should be 3 characters",
+            "name.max"=>"Name maximum should be 10 Characters",
+            "city.required"=>"City is Required",
+            // "city.uppercase"=>"Please Capital Letters.",
+            "phone.required"=>"Phone is required",
+            "phone.min"=>"Phone min should be 10 characters",
+            "phone.max"=>"Phone max should be 11 characters",
+            "skill.required"=>"skill is required",
+            "age.required"=>"Age is required"
         ]);
        return $req->input();
 
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -11,20 +11,36 @@
 
     <form action="" method="post">
         @csrf
-        <input type="text" name="name" placeholder="Name">
-        <p>@error("name"){{$message}}@enderror</p>
-        <input type="text" name="phone" placeholder="Phone">
-        <p>@error("phone"){{$message}}@enderror</p>
-        <input type="text" name="city" placeholder="City">
-        <p>@error("city"){{$message}}@enderror</p>
+        <input type="text" name="name" value="{{old("name")}}" placeholder="Name">
+        <p>
+            @error('name')
+                {{ $message }}
+            @enderror
+        </p>
+        <input type="text" name="phone" value="{{old("phone")}}" placeholder="Phone">
+        <p>
+            @error('phone')
+                {{ $message }}
+            @enderror
+        </p>
+        <input type="text" name="city" value="{{old("city")}}" placeholder="City">
+        <p>
+            @error('city')
+                {{ $message }}
+            @enderror
+        </p>
         <input type="checkbox" value="PHP" name="skill[]" id="php">
         <label for="php">PHP</label>
         <input type="checkbox" value="Node" name="skill[]" id="node">
         <label for="node">Node</label>
         <input type="checkbox" value="React" name="skill[]" id="react">
         <label for="react">React</label>
-        <p>@error("skill"){{$message}}@enderror</p>
-        
+        <p>
+            @error('skill')
+                {{ $message }}
+            @enderror
+        </p>
+
 
         <label>Gender</label>
         <br />
@@ -32,12 +48,20 @@
         <label for="male">Male</label>
         <input type="radio" name="gender" value="Female" id="female">
         <label for="female">Female</label>
-        <p>@error("gender"){{$message}}@enderror</p>
+        <p>
+            @error('gender')
+                {{ $message }}
+            @enderror
+        </p>
         <br />
         <label>Age</label>
         <input type="range" name="age" min="18" max="25">
-        <p>@error("age"){{$message}}@enderror</p>
-       
+        <p>
+            @error('age')
+                {{ $message }}
+            @enderror
+        </p>
+
 
         <button>Submit</button>
     </form>
