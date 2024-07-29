a5053904c2dd1cf5d09d282b75c54080eef4e62c
Akif Ullah <akifullah0317@gmail.com>
2024-07-28 22:09:20 -0700

    Form handling with checkbox

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -10,6 +10,7 @@ class FormController extends Controller
 
     function add(Request $req){
        return $req->input();
+       
     // echo $req->name;
     // echo $req->phone;
     // echo $req->city;
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -8,6 +8,27 @@
         <input type="text" name="name" placeholder="Name" ><br/>
         <input type="text" name="phone" placeholder="Phone" ><br/>
         <input type="text" name="city" placeholder="City" ><br/>
+        <input type="checkbox" value="PHP" name="skill[]" id="php" >
+        <label for="php">PHP</label>
+        <input type="checkbox" value="Node" name="skill[]" id="node" >
+        <label for="node">Node</label>
+        <input type="checkbox" value="React" name="skill[]" id="react" >
+        <label for="react">React</label>
+
+        <br/>
+        <br/>
+
+        <label>Gender</label>
+        <br/>
+        <input type="radio" name="gender" value="Male" id="male">
+        <label for="male">Male</label>
+        <input type="radio" name="gender" value="Female" id="female">
+        <label for="female">Female</label>
+        <br/>
+        <label>Age</label>
+        <input type="range" name="age" min="18" max="25">
+        <br/>
+        
         <button>Submit</button>
     </form>
     
