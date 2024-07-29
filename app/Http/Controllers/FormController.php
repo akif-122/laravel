5ed3ae6bf3c2e40d31954d2977d974cc69991358
Akif Ullah <akifullah0317@gmail.com>
2024-07-28 22:41:49 -0700

    Learn form validation

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -9,8 +9,15 @@ class FormController extends Controller
     //
 
     function add(Request $req){
+        $req->validate([
+            "name"=> "required | min:3 | max:10 ",
+            "city"=> "required  ",
+            "phone"=> "required | min:10 | max: 11",
+            "skill"=> "required",
+            "age"=> "required ",
+        ]);
        return $req->input();
-       
+
     // echo $req->name;
     // echo $req->phone;
     // echo $req->city;
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -3,34 +3,44 @@
 
     <h1>Form</h1>
 
-    <form action="" method="post" >
-        @csrf
-        <input type="text" name="name" placeholder="Name" ><br/>
-        <input type="text" name="phone" placeholder="Phone" ><br/>
-        <input type="text" name="city" placeholder="City" ><br/>
-        <input type="checkbox" value="PHP" name="skill[]" id="php" >
-        <label for="php">PHP</label>
-        <input type="checkbox" value="Node" name="skill[]" id="node" >
-        <label for="node">Node</label>
-        <input type="checkbox" value="React" name="skill[]" id="react" >
-        <label for="react">React</label>
+    {{-- @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    @endif --}}
 
-        <br/>
-        <br/>
+    <form action="" method="post">
+        @csrf
+        <input type="text" name="name" placeholder="Name">
+        <p>@error("name"){{$message}}@enderror</p>
+        <input type="text" name="phone" placeholder="Phone">
+        <p>@error("phone"){{$message}}@enderror</p>
+        <input type="text" name="city" placeholder="City">
+        <p>@error("city"){{$message}}@enderror</p>
+        <input type="checkbox" value="PHP" name="skill[]" id="php">
+        <label for="php">PHP</label>
+        <input type="checkbox" value="Node" name="skill[]" id="node">
+        <label for="node">Node</label>
+        <input type="checkbox" value="React" name="skill[]" id="react">
+        <label for="react">React</label>
+        <p>@error("skill"){{$message}}@enderror</p>
+        
 
         <label>Gender</label>
-        <br/>
+        <br />
         <input type="radio" name="gender" value="Male" id="male">
         <label for="male">Male</label>
         <input type="radio" name="gender" value="Female" id="female">
         <label for="female">Female</label>
-        <br/>
+        <p>@error("gender"){{$message}}@enderror</p>
+        <br />
         <label>Age</label>
         <input type="range" name="age" min="18" max="25">
-        <br/>
-        
+        <p>@error("age"){{$message}}@enderror</p>
+       
+
         <button>Submit</button>
     </form>
-    
-     
+
+
 </div>
